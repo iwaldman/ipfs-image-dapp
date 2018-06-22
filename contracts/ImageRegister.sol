9e297da2979d3367b8317abfede7f3e9ad1bdd68
Irvin M. Waldman <irvin.waldman@hotmail.com>
2018-06-22 10:17:34 -0500

    Add contract comments

--- contracts/ImageRegister.sol
+++ contracts/ImageRegister.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.4.24;
 
 // Base contract that can be destroyed by owner. 
-// All funds in contract will be sent to the owner.
 import "openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
 
+/**
+ * @title ImageRegister
+ * @author Irvin M. Waldman (@irvinwaldman)
+ * @dev Stores IPFS (multihash) hash by address along with metadata
+ */
 contract ImageRegister is Destructible {
   struct Image {
     string ipfsHash;
@@ -24,7 +28,13 @@ contract ImageRegister is Destructible {
     string _metadata
   );
 
-  // Store an image IPFS hash and metadata
+   /**
+   * @dev associate a image entry with the sender address
+   * @param _ipfsHash IPFS hash
+   * @param _title image title
+   * @param _description image description
+   * @param _metadata image tag(s)
+   */
   function setImage(
     string _ipfsHash, 
     string _title, 
@@ -52,11 +62,24 @@ contract ImageRegister is Destructible {
     _success = true;
   }
 
+  /**
+   * @dev returns the number of images associated with the given address
+   * @param _owner owner address
+   * @return number of images associated with a given address
+   */
   function getImageCount(address _owner) public view returns (uint256) {
     return ownerToImages[_owner].length;
   }
 
-  // Return an owner's IPFS hashes 
+  /**
+   * @dev returns the 
+   * @param _owner owner address
+   * @param _index next image to return
+   * @return _ipfsHash IPFS hash
+   * @return _title image title
+   * @return _description image description
+   * @return _metadata image metadata
+   */ 
   function getImage(address _owner, uint8 _index) public view returns (
     string _ipfsHash, 
     string _title, 
