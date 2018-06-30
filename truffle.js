b3d6c1c5a9199e27b4c35f0f315269b1873dd22b
Irvin M. Waldman <irvin.waldman@hotmail.com>
2018-06-29 21:32:25 -0500

    Change provide to function

--- truffle.js
+++ truffle.js
@@ -9,38 +9,37 @@ module.exports = {
       port: 8545,
       network_id: '*', // Match any network id
     },
-    // testnets
-    // properties
-    // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
-    // gas: gas limit
-    // gasPrice: gas price in gwei
     ropsten: {
-      provider: new HDWalletProvider(
-        process.env.MNENOMIC,
-        'https://ropsten.infura.io/' + process.env.INFURA_API_KEY
-      ),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          'https://ropsten.infura.io/' + process.env.INFURA_API_KEY
+        ),
       network_id: 3,
     },
     kovan: {
-      provider: new HDWalletProvider(
-        process.env.MNENOMIC,
-        'https://kovan.infura.io/' + process.env.INFURA_API_KEY
-      ),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          'https://kovan.infura.io/' + process.env.INFURA_API_KEY
+        ),
       network_id: 42,
     },
     rinkeby: {
-      provider: new HDWalletProvider(
-        process.env.MNENOMIC,
-        'https://rinkeby.infura.io/' + process.env.INFURA_API_KEY
-      ),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          'https://rinkeby.infura.io/' + process.env.INFURA_API_KEY
+        ),
       network_id: 4,
     },
     // main ethereum network(mainnet)
     main: {
-      provider: new HDWalletProvider(
-        process.env.MNENOMIC,
-        'https://mainnet.infura.io/' + process.env.INFURA_API_KEY
-      ),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          'https://mainnet.infura.io/' + process.env.INFURA_API_KEY
+        ),
       network_id: 1,
     },
   },
